a5e7a21bc37518af1c88901f6b01ac0320b497e1
mishabeliy15 <misha.beliy15@gmail.com>
2017-09-06 00:47:46 +0300

    begin and tests

--- electric_shema/Form1.Designer.cs
+++ electric_shema/Form1.Designer.cs
@@ -28,12 +28,133 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Schema = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.resistor = new System.Windows.Forms.RadioButton();
+            this.Elemets = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.Elemets.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Schema
+            // 
+            this.Schema.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Schema.Location = new System.Drawing.Point(170, 41);
+            this.Schema.Name = "Schema";
+            this.Schema.Size = new System.Drawing.Size(600, 600);
+            this.Schema.TabIndex = 9;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(796, 78);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(27, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Добавить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(44, 21);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // resistor
+            // 
+            this.resistor.AutoSize = true;
+            this.resistor.Checked = true;
+            this.resistor.Location = new System.Drawing.Point(21, 37);
+            this.resistor.Name = "resistor";
+            this.resistor.Size = new System.Drawing.Size(17, 16);
+            this.resistor.TabIndex = 7;
+            this.resistor.TabStop = true;
+            this.resistor.UseVisualStyleBackColor = true;
+            // 
+            // Elemets
+            // 
+            this.Elemets.Controls.Add(this.radioButton2);
+            this.Elemets.Controls.Add(this.radioButton1);
+            this.Elemets.Controls.Add(this.resistor);
+            this.Elemets.Controls.Add(this.pictureBox2);
+            this.Elemets.Location = new System.Drawing.Point(12, 41);
+            this.Elemets.Name = "Elemets";
+            this.Elemets.Size = new System.Drawing.Size(152, 500);
+            this.Elemets.TabIndex = 9;
+            this.Elemets.TabStop = false;
+            this.Elemets.Text = "Элементы схемы";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(21, 145);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(17, 16);
+            this.radioButton2.TabIndex = 9;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(21, 98);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(17, 16);
+            this.radioButton1.TabIndex = 8;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(908, 602);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Elemets);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Schema);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.Elemets.ResumeLayout(false);
+            this.Elemets.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.Panel Schema;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.RadioButton resistor;
+        private System.Windows.Forms.GroupBox Elemets;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
     }
 }
 
--- electric_shema/Form1.cs
+++ electric_shema/Form1.cs
@@ -7,14 +7,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json;
 
 namespace electric_shema
 {
     public partial class Form1 : Form
     {
+        PictureBox[,] kletka;
         public Form1()
         {
             InitializeComponent();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /*Graphics g = this.CreateGraphics();
+            g.DrawLine(new Pen(Color.Red), 10, 10, 100, 100);*/
+            //contextMenuStrip1.Show(MousePosition, ToolStripDropDownDirection.Right);
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            kletka = new PictureBox[10, 10];
+            for (int i = 0; i < 10; i++)
+                for (int j = 0; j < 10; j++)
+                {
+                    kletka[i, j] = new PictureBox();
+                    kletka[i, j].BorderStyle = BorderStyle.FixedSingle;
+                    kletka[i, j].Size = new Size(50, 50);
+                    kletka[i, j].Location = new Point(50 * j, 50 * i);
+                    kletka[i, j].SizeMode = PictureBoxSizeMode.StretchImage;
+                    this.Schema.Controls.Add(kletka[i, j]);
+                }
         }
     }
 }
